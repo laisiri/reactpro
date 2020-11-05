4ec5fb1d257e5d1c8aa1f5688c8cebf3352e263e
laisiri <thonyang08@gmail.com>
2020-11-05 20:55:19 +0700

    react component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name},you are {props.age} years old</p>
+    </div>
+  )
+}
 
-  <div>
+const App = () => {
+
+  const name='Peter';
+  const age=20;
+
   
-    <p>Hello World!</p>
-    
-  </div>
-)
-
+  return (
+    <div>
+      <h1>Greeting</h1>
+      < Hello name='Geoge' age={26+9}/>
+      < Hello name={name} age={age}/>
+      
+    </div>
+  );
+}
 ReactDOM.render(<App />, document.getElementById('root')
 );
 

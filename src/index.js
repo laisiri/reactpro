7c8865466b03a41708629fd96268080a688edb15
laisiri <thonyang08@gmail.com>
2020-11-07 23:16:47 +0700

    setinterval

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name},you are {props.age} years old</p>
-    </div>
-  )
-}
-
-const App = () => {
-
-  const name='Peter';
-  const age=20;
-
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date:new Date()};
+   
+  }
   
-  return (
-    <div>
-      <h1>Greeting</h1>
-      < Hello name='Geoge' age={26+9}/>
-      < Hello name={name} age={age}/>
-      
-    </div>
-  );
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.tick(); 
+    },1000) 
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick = () =>  this.setState({date: new Date()})
+  
+  render() {
+    
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <p>It's time {this.state.date.toLocaleTimeString()}.</p>
+        <button type = 'button' 
+                onClick={this.componentWillUnmount.bind(this)}>stop timer</button>
+      </div>
+
+    )
+  }
 }
-ReactDOM.render(<App />, document.getElementById('root')
+ReactDOM.render(<Clock />, document.getElementById('root')
 );
 
